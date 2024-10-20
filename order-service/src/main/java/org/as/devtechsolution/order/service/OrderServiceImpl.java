0566c6d0a5875e82be0ad1cb0bb790af70de0da3
devtechsolution <46072271+devtechsolution@users.noreply.github.com>
2024-10-20 21:57:03 +0530

    EC-4 Inter communication between services using feign client and stubbing using wiremock

--- order-service/src/main/java/org/as/devtechsolution/order/service/OrderServiceImpl.java
+++ order-service/src/main/java/org/as/devtechsolution/order/service/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package org.as.devtechsolution.order.service;
 
 import lombok.AllArgsConstructor;
+import org.as.devtechsolution.order.client.InventoryClient;
 import org.as.devtechsolution.order.dto.OrderRequest;
 import org.as.devtechsolution.order.model.Order;
 import org.as.devtechsolution.order.repository.OrderRepository;
@@ -16,20 +17,25 @@ import java.util.UUID;
 public class OrderServiceImpl implements  OrderService{
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     @Override
     public void placeOrder(final OrderRequest orderRequest) {
+        final var inStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (inStock) {
+            //map orderRequest to order object
+            Order order = Order.builder()
+                    .orderNumber(UUID.randomUUID().toString())
+                    .price(orderRequest.price())
+                    .skuCode(orderRequest.skuCode())
+                    .quantity(orderRequest.quantity())
+                    .build();
+            //save order to orderRepository
+            final var saveOrder = orderRepository.save(order);
 
-        //map orderRequest to order object
-        Order order= Order.builder()
-                .orderNumber(UUID.randomUUID().toString())
-                .price(orderRequest.price())
-                .skuCode(orderRequest.skuCode())
-                .quantity(orderRequest.quantity())
-                .build();
-        //save order to orderRepository
-        final var saveOrder = orderRepository.save(order);
 
-
+        } else {
+            throw new RuntimeException("Product with SkuCode "+orderRequest.skuCode()+" is not in stock");
+        }
     }
 }
--- order-service/src/test/java/org/as/devtechsolution/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/org/as/devtechsolution/order/OrderServiceApplicationTests.java
@@ -1,15 +1,80 @@
 package org.as.devtechsolution.order;
 
+import io.restassured.RestAssured;
+import org.as.devtechsolution.order.stubs.InventoryClientStub;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
+	@LocalServerPort
+	private Integer port;
+	@Autowired
+	private TestcontainersConfiguration testcontainersConfiguration;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+		testcontainersConfiguration.mysqlContainer().start();
+
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldSubmitOrder() {
+		String submitOrderJson = """
+                {
+                     "skuCode": "iphone_15",
+                     "price": 1000,
+                     "quantity": 1
+                }
+                """;
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
+
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+
+		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
+	}
+
+	@Test
+	void shouldFailOrderWhenProductIsNotInStock() {
+		String submitOrderJson = """
+                {
+                     "skuCode": "iphone_15",
+                     "price": 1000,
+                     "quantity": 1000
+                }
+                """;
+		InventoryClientStub.stubInventoryCall("iphone_15", 1000);
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(500);
 	}
 
 }
